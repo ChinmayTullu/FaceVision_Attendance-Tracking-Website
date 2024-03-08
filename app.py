fe4b83e8300e3cfdc41f589ee4089628cb9ed51a
Chinmay Milind Tullu <chinmaytullu15@gmail.com>
2024-03-08 21:07:46 +0530

    update accuracy

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ def recognize():
                 id, confidence=recognizer.predict(gray[y:y+h,x:x+w]) 
 
                 #check if confidence is less than 100 ==> "0" is perfect match 
-                if (confidence < 70): #if the picture is recognised             
+                if (confidence < 60): #if the picture is recognised             
                     id = names[id]
                     confidence = "  {0}%".format(round(100 - confidence))
                     
